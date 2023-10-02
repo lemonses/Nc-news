4458325fa9f7ab95b7fe6c9396b7fa0ddf828697
Waldo <wilfdavidjames@gmail.com>
2023-10-02 11:51:09 +0100

    added more testing functionality

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 const seed = require('../db/seeds/seed')
 const data = require('../db/data/test-data')
 
+beforeAll(()=>seed(data))
 afterAll(()=>db.end())
 
 describe('/api',()=>{
@@ -29,6 +30,7 @@ describe('GET /api/topics',()=>{
         .get('/api/topics')
         .expect(200)
         .then(({body})=>{
+            expect(body.topics).toHaveLength(3)
             body.topics.forEach(topic => {
                 expect(topic).toHaveProperty('slug')
                 expect(topic).toHaveProperty('description')
