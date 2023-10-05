aede00cceb911446667c10b9b08a0aeeb5e6ccec
Waldo <wilfdavidjames@gmail.com>
2023-10-05 14:34:02 +0100

    refactored to use routers

--- server/app.js
+++ server/app.js
@@ -1,25 +1,12 @@
 const express = require('express')
-const {getArticle,getArticles,getComments,patchArticle,deleteComment,postComment} = require('./controllers/articles.controller.js')
 const {handleCustomErrors, handlePSQLErrors,handle500} = require('./controllers/errors.controller.js')
-const {getTopics,getApi} = require('./controllers/topics.controller.js')
-const {getUsers} = require('./controllers/users.controllers.js')
+const apiRouter = require('./routes/api-router.js')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/api',getApi)
-app.get('/api/topics',getTopics)
-app.get('/api/articles',getArticles)
-app.get('/api/articles/:article_id',getArticle)
-app.get('/api/articles/:article_id/comments',getComments)
-app.get('/api/users',getUsers)
-
-app.patch('/api/articles/:article_id',patchArticle)
-
-app.delete('/api/comments/:comment_id',deleteComment)
-
-app.post('/api/articles/:article_id/comments',postComment)
+app.use('/api',apiRouter)
 
 app.use(handleCustomErrors)
 app.use(handlePSQLErrors)
