c2f36b12db4f7fb71fd7bdb5f2ac76fd96f41249
Waldo <wilfdavidjames@gmail.com>
2023-10-06 16:29:03 +0100

    removed server logs

--- server/controllers/errors.controller.js
+++ server/controllers/errors.controller.js
@@ -5,7 +5,6 @@ exports.handleCustomErrors = (err,req,res,next) => {
 }
 
 exports.handlePSQLErrors = (err,req,res,next) => {
-    console.log(err)
     if(err.code === '22P02' || '23503' || '23502'){
         res.status(400).send({ message: 'Bad request' })
     }else {next(err)}
