047b8726a308e95c44281941d853e3b327ca08de
Waldo <wilfdavidjames@gmail.com>
2023-10-06 11:27:06 +0100

    added console logs for testing deployment

--- server/controllers/errors.controller.js
+++ server/controllers/errors.controller.js
@@ -1,10 +1,12 @@
 exports.handleCustomErrors = (err,req,res,next) => {
+    console.log(err)
     if(err.status){
         res.status(err.status).send({message:err.message})
     } else {next(err)}
 }
 
 exports.handlePSQLErrors = (err,req,res,next) => {
+    console.log(err)
     if(err.code === '22P02' || '23503' || '23502'){
         res.status(400).send({ message: 'Bad request' })
     }else {next(err)}
